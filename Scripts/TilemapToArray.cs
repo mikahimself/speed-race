0931c5eddda8181b13fd65307d45fbc7574e01f4
Mika Laihanen <mika.laihanen@basware.com>
2020-08-03 16:02:47 +0300

    Add tilemap tool

---
+++ Scripts/TilemapToArray.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public class TilemapToArray : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    TileMap tiles;
+    bool readout = false;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        tiles = (TileMap)GetNode("TileMap");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+    if (Input.IsActionPressed("ui_accept") && !readout)
+    {
+        readout = true;
+        var xline = "";
+        for (int y = 0; y < 15; y++)
+        {
+            for (int x = 0; x < 15; x++)
+            {
+                if (tiles.GetCell(x, y) != -1) {
+                    xline += tiles.GetCell(x, y) + ", ";
+                }
+            }
+            GD.Print(xline);
+            xline = "";
+        }
+    }    
+  }
+}
